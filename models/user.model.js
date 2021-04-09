e7a08cf0d7b87464b9356dd1f4018d18ada6725a
2ndyrGroupA <muchizukishirou@gmail.com>
2021-04-09 15:00:59 +0800

    Innitial backend

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    occupation: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    phonenumber: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+},{
+    timestamp: true
+}); 
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
